f320f9cc53d8749d62e4416d2324c8495177a25f
fkkt <admin@catcompany.cn>
2023-07-20 15:55:00 +0800

    update build version

--- common/src/main/java/cc/imorning/common/constant/ServerConfig.kt
+++ common/src/main/java/cc/imorning/common/constant/ServerConfig.kt
@@ -1,27 +1,29 @@
 package cc.imorning.common.constant
 
+import cc.imorning.common.BuildConfig
+
 /**
  * This class saved configure of chat sever,modify them for your own server
  */
 object ServerConfig {
 
     /**
-     * Host name,typically is domain of server
+     * Host name, typically is domain of server
      */
-    const val HOST_NAME = "im.imorning.cc"
+    val HOST_NAME = BuildConfig.host_name
 
     /**
      * Same as [HOST_NAME]
      */
-    const val DOMAIN = "im.imorning.cc"
+    val DOMAIN = BuildConfig.domain
 
     /**
-     * Resource of client,different servers have different resources
+     * Resource of client, different servers have different resources
      */
-    const val RESOURCE = "android"
+    val RESOURCE = BuildConfig.resource
 
     /**
-     * Login port,default is 5222
+     * Login port, default is 5222
      */
-    const val LOGIN_PORT = 5222
+    val LOGIN_PORT = BuildConfig.port
 }
